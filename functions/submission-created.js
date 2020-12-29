41bffd2619f6cec1d30450455e111aab3daa92bf
Carolanne <carolanne.fuchs@gmail.com>
2020-12-29 01:06:19 +0000

    Invoke Python

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -2,16 +2,17 @@ exports.handler = function (event, context) {
     const spawn = require("child_process").spawn;
 
     //console.log(event, context)
-    console.log("Event: ", event)
-    console.log("====================")
-    console.log("Context: ", context)
-    console.log("====================")
-    console.log("====================")
-    console.log("====================")
-    console.log('print ', `Hello ${JSON.parse(event.body).payload.name}`)
-    //const pythonProcess = spawn('python',["print", `Hello ${JSON.parse(event.body).payload.name}`]);
+    //console.log("Event: ", event)
+    // console.log("====================")
+    // console.log("Context: ", context)
+    // console.log("====================")
+    // console.log("====================")
+    // console.log("====================")
+    // console.log('print ', `Hello ${JSON.parse(event.body).payload.name}`)
+    const pythonProcess = spawn('python',["print", `Hello ${JSON.parse(event.body).payload.name}`]);
+    console.log(pythonProcess)
     return {
         statusCode: 200,
-        //body: `Response: ${pythonProcess}`
+        body: `Response: ${pythonProcess}`
     }
 }
