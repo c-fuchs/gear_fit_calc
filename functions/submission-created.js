45a61d7cc57196c74c88312cc7b3189447c51766
Carolanne <carolanne.fuchs@gmail.com>
2020-12-29 01:00:33 +0000

    Submit function

---
+++ functions/submission-created.js
@@ -0,0 +1,17 @@
+exports.handler = function (event, context) {
+    const spawn = require("child_process").spawn;
+
+    //console.log(event, context)
+    console.log("Event: ", event)
+    console.log("====================")
+    console.log("Context: ", context)
+    console.log("====================")
+    console.log("====================")
+    console.log("====================")
+    console.log('print ', `Hello ${JSON.parse(event.body).payload.name}`)
+    //const pythonProcess = spawn('python',["print", `Hello ${JSON.parse(event.body).payload.name}`]);
+    return {
+        statusCode: 200,
+        //body: `Response: ${pythonProcess}`
+    }
+}
